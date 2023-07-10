795847e5092611cd774837644e60055dcc93daa5
Emmanuel Um <johndoeexample.com>
2023-07-10 10:52:36 +0200

    First of the search application

---
+++ project/NutriFood/search/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hey ! tu es sur l'index de l'application search du projet NutriFood.")
