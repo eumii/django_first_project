40400d602d2350c6a4b7d4ef13742f5d4f487306
Emmanuel Um <johndoeexample.com>
2023-07-11 10:08:19 +0200

    ajout de la base de données post gresql

--- project/NutriFood/NutriFood/settings.py
+++ project/NutriFood/NutriFood/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'zum2fr',
+        'PASSWORD': 'db_for_django',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
